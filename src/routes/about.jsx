5822939d7ddce77967ef9e591a982479be580797
hi master <54254502+ohyoungil@users.noreply.github.com>
2021-02-16 20:48:25 +0900

    Create about.jsx

---
+++ src/routes/about.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './about.css';
+
+
+function About() {
+  return (
+    <div className="about__container">
+      <span>About this page: 리액트를 사용해서 movie 웹사이트 만들기</span>
+    </div>
+  ) 
+}
+
+export default About;
